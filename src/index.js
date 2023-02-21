cc6da06d1e4a39f881f0d78c8ebb7c428e62de6f
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2023-02-21 14:28:58 -0800

    asdfads

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const socketio = require("socket.io");
 const app = express();
 require("dotenv").config();
+
 const port = process.env.PORT;
 const path = require("path");
+
 const publicPath = path.join(__dirname, "../public");
+
+const http = require("http");
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("New WebSocket connection");
+});
+
+server.listen(port, () => {
   console.log("Server is running on port 3000");
 });
