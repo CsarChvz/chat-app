050e9092a90fbf49806d2ef54e01d093c1c4c7fb
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2023-02-22 09:01:53 -0800

    Socket emit, on y broadcast

--- src/index.js
+++ src/index.js
@@ -1,21 +1,36 @@
-const path = require('path')
-const http = require('http')
-const express = require('express')
-const socketio = require('socket.io')
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
 
-const app = express()
-const server = http.createServer(app)
-const io = socketio(server)
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname, '../public')
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "../public");
 
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
-    console.log('New WebSocket connection')
-})
+io.on("connection", (socket) => {
+  console.log("New WebSocket connection");
+
+  socket.emit("message", "Welcome!");
+  socket.broadcast.emit("message", "A new user has joined!");
+
+  socket.on("sendMessage", (message) => {
+    io.emit("message", message);
+  });
+
+  // Evento de desconexiób
+
+  socket.on("disconnect", () => {
+    // Como ya el socket se desconectó, no podemos usar el socket.emit, sino que usamos io.emit
+    // Para enviar el mensaje a todos los usuarios que la persona se desconecto
+    io.emit("message", "A user has left!");
+  });
+});
 
 server.listen(port, () => {
-    console.log(`Server is up on port ${port}!`)
-})
+  console.log(`Server is up on port ${port}!`);
+});
