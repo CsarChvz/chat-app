e8b7a6bfef93555ceb159941107577e81d438a05
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2023-02-22 10:02:11 -0800

    Estados en el formulario y botones

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,20 +1,42 @@
 const socket = io();
 
+// Message -- Elementos del DOM
+
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+
+// Location -- Elementos del DOM
+const $sendLocationButton = document.querySelector("#send-location");
+
 socket.on("message", (message) => {
   console.log(message);
 });
 
-document.querySelector("#message-form").addEventListener("submit", (e) => {
+$messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  $messageFormButton.setAttribute("disabled", "disabled");
 
+  // Disable the form
   const message = e.target.elements.message.value;
 
-  socket.emit("sendMessage", message, (mesage) => {
+  socket.emit("sendMessage", message, (error) => {
+    // Enable the form
+
+    // Se remueve el atributo disabled del boton
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+    if (error) {
+      return console.log(error);
+    }
+
     console.log("Message delivered!");
   });
 });
 
-document.querySelector("#send-location").addEventListener("click", () => {
+$sendLocationButton.addEventListener("click", () => {
+  $sendLocationButton.setAttribute("disabled", "disabled");
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser.");
   }
@@ -25,7 +47,11 @@ document.querySelector("#send-location").addEventListener("click", () => {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude,
       },
-      () => {
+      (error) => {
+        if (error) {
+          return console.log(error);
+        }
+        $sendLocationButton.removeAttribute("disabled");
         console.log("Location shared!");
       }
     );
